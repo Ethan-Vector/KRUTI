d56537e061525c361fd67d964ca3896230c40959
Ethan Vector <valvod486@gmail.com>
2025-11-25 18:33:20 +0100

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,5 @@
+class KrutiAgent:
+    def __init__(self):
+        pass
+    def run(self):
+        print('KRUTI agent running (Full Pro scaffold)')
---
+++ main.py
@@ -0,0 +1,5 @@
+from agent import KrutiAgent
+
+if __name__=='__main__':
+    agent=KrutiAgent()
+    agent.run()
